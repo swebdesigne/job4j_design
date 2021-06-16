62c16cd5c9faba42df3ba017196e7c09de95886c
Igor <swebdesigne@gmail.com>
2021-06-16 20:20:00 +0300

    Динамический список на массиве

--- src/main/java/collection/SimpleArray.java
+++ src/main/java/collection/SimpleArray.java
@@ -1,34 +1,62 @@
 package collection;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Objects;
+import java.util.*;
 
 public class SimpleArray<T> implements Iterable {
     private Object[] container;
-    private int size = 0;
+    private int size;
+    private int capacity;
+    private int modCount = 0;
     private int index = 0;
-    private int change = 0;
+
+    public SimpleArray() {
+        this.container = new Object[2];
+        this.size = 0;
+        this.capacity = 2;
+    }
 
     public T get(int index) {
         Objects.checkIndex(index, size);
         return (T) container[index];
     }
 
+    /**
+     * Метод добавляет модель в конец массива
+     * @param model
+     */
     public void add(T model) {
-        Objects.checkIndex(index, size);
-        container[index] = model;
-        index++;
-        change++;
+        if (size == capacity) {
+            capacity(2);
+        }
+        container[size] = model;
+        size++;
+        modCount++;
+    }
+
+    /**
+     * Метод увеличивает размер capacity
+     * @param v
+     */
+    private void capacity(double v) {
+        Object[] temp = new Object[(int) (this.capacity * v)];
+        for (int i = 0; i < capacity; i++) {
+            temp[i] = container[i];
+        }
+        container = temp;
+        capacity = (int) (capacity * v);
     }
 
     @Override
     public Iterator<T> iterator() {
-        int expectedModCount = index;
+        int expectedModcount = modCount;
+        System.out.println(expectedModcount + " === " + modCount);
+        if (modCount != expectedModcount) {
+            throw new ConcurrentModificationException();
+        }
         return new Iterator<T>() {
             @Override
             public boolean hasNext() {
-                return index < size;
+                return size < capacity;
             }
 
             @Override
--- src/test/java/collection/SimpleArrayTest.java
+++ src/test/java/collection/SimpleArrayTest.java
@@ -3,6 +3,10 @@ package collection;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -23,4 +27,33 @@ public class SimpleArrayTest {
         String rsl = array.iterator().next();
         assertThat(rsl, is("first"));
     }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetEmpty() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.get(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetOutBound() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("first");
+        array.get(1);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenGetEmptyFromIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        System.out.println(array.iterator().next());
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("first");
+        Iterator<String> it = array.iterator();
+        array.add("second");
+        it.next();
+    }
+
 }
