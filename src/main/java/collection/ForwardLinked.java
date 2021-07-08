477e18d5be6d64c6aa13433b4dc927adb6bf3c58
Igor <swebdesigne@gmail.com>
2021-07-08 16:41:10 +0300

    Перевернуть связанный список

--- src/main/java/collection/ForwardLinked.java
+++ src/main/java/collection/ForwardLinked.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable {
     private Node<T> head;
+    private int size = 0;
 
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
@@ -17,12 +18,14 @@ public class ForwardLinked<T> implements Iterable {
             tail = tail.next;
         }
         tail.next = node;
+        size++;
     }
 
     public void addFirst(T value) {
         Node<T> node = new Node<T>(value, null);
         node.next = head;
         head = node;
+        size++;
     }
 
     public T deleteFirst() {
@@ -31,7 +34,21 @@ public class ForwardLinked<T> implements Iterable {
         }
         T node = head.value;
         head = head.next;
+        size--;
         return node;
+    }
+
+    public boolean revert() {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        Node<T> tmp = head;
+        while (tmp.equals(head)) {
+            tmp = head;
+            head = head.next;
+            head.next = tmp;
+        }
+        return true;
     }
 
     @Override
