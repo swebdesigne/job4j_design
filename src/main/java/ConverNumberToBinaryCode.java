bfe3c0991cc98965f2c149e6fcc3286bce1c1104
Igor <swebdesigne@gmail.com>
2021-07-13 13:28:50 +0300

    Convert the number to binary code

---
+++ src/main/java/ConverNumberToBinaryCode.java
@@ -0,0 +1,15 @@
+public class ConverNumberToBinaryCode {
+    public static String binary(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 31; i++) {
+            sb.append(num % 2 == 0 ? 0 : 1);
+            sb.append((i + 1) % 8 == 0 ? " " : "");
+            num /= 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binary(16));
+    }
+}
