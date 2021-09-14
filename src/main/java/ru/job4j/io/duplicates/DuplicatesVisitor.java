6c17fa37857efd3615c3b12119b7fa8227bbbca8
Igor <swebdesigne@gmail.com>
2021-09-14 12:22:57 +0300

    Поиск дубликатов

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,22 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Set<FileProperty> set = new HashSet<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (!attrs.isDirectory()) {
+            if (!set.add(new FileProperty(attrs.size(), String.valueOf(file.getFileName())))) {
+                System.out.println(String.format("fileName = %s size = %s", file.getFileName(), attrs.size()));
+            }
+        }
+        return super.visitFile(file, attrs);
+    }
+}
