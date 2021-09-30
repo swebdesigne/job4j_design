fe0da5edd68db45a9bd0e0cf9b102398cb7139d9
beelk <swebdesigne@gmail.com>
2021-09-30 13:32:00 +0300

    Scanner
    Signed-off-by: beelk <swebdesigne@gmail.com>

--- src/main/java/ru/job4j/io/csvstrategy/OutputToFile.java
+++ src/main/java/ru/job4j/io/csvstrategy/OutputToFile.java
@@ -2,8 +2,6 @@ package ru.job4j.io.csvstrategy;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class OutputToFile implements CSVWrite {
     @Override
