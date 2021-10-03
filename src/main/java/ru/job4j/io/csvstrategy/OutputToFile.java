a6950257f862bfcbfdb465e59ed840d0b752ee05
beelk <swebdesigne@gmail.com>
2021-10-03 18:45:29 +0300

    Scanner
    Signed-off-by: beelk <swebdesigne@gmail.com>

--- src/main/java/ru/job4j/io/csvstrategy/OutputToFile.java
+++ src/main/java/ru/job4j/io/csvstrategy/OutputToFile.java
@@ -4,11 +4,12 @@ import ru.job4j.io.CSVReader;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class OutputToFile implements CSVWrite {
     @Override
     public void output(CSVReader result) {
-        try (FileWriter writer = new FileWriter(String.valueOf(result.getOut()))) {
+        try (FileWriter writer = new FileWriter(String.valueOf(result.getOut()), StandardCharsets.UTF_8)) {
             writer.write(String.valueOf(result.getResult()));
         } catch (IOException e) {
             e.printStackTrace();
