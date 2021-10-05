00ed993245da65ef481ded8d2ea515740ebd54c5
beelk <swebdesigne@gmail.com>
2021-10-05 17:30:57 +0300

    Бот
    Signed-off-by: beelk <swebdesigne@gmail.com>

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -9,11 +9,12 @@ import java.net.Socket;
 
 public class EchoServer {
     private static final String BYE = "Bye";
+    private static final String HELLO = "Hello";
 
-    private boolean parseString(String str, String check) {
+    private String parseString(String str) {
         String[] arr = str.split("=");
         String[] subArr = arr[1].split(" ");
-        return subArr[0].equals(check);
+        return subArr[0];
     }
 
     public static void main(String[] args) {
@@ -24,15 +25,21 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("Hello, dear friend.".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("=")) {
-                            if (echoServer.parseString(str, BYE)) {
+                        if (str.contains("msg=")) {
+                            String result = echoServer.parseString(str);
+                            if (result.equals(BYE)) {
                                 server.close();
+                            } else if (result.equals(HELLO)) {
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write(String.valueOf("\"" + result + "\"").getBytes());
+                            } else {
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write("\"What?\"\r\n".getBytes());
                             }
                         }
-                        System.out.println(str);
                     }
-                    out.flush();
                 }
             }
         } catch (IOException e) {
