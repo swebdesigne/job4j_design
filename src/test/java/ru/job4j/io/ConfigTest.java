d87d0b30ab003be03c7c529b447e33d7d7abdb07
beelk <swebdesigne@gmail.com>
2021-10-13 13:08:19 +0300

    удаляем однострочные комменты
    Signed-off-by: beelk <swebdesigne@gmail.com>

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -9,7 +9,7 @@ import static org.hamcrest.core.Is.*;
 public class ConfigTest {
     @Test
     public void whenPairWithoutComment() {
-        String path = "src/main/java/ru/job4j//data/pair_without_comment.properties";
+        String path = "src/main/java/ru/job4j/data/pair_without_comment.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("name"), is("Igor Sivolobov"));
