4ecf4a155c9b8e1bbab33c1cad6cbb6276b6cce9
beelk <swebdesigne@gmail.com>
2021-10-13 13:11:26 +0300

    Slf4j - вывод переменных
    Signed-off-by: beelk <swebdesigne@gmail.com>

---
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsageLog4j {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        char tChar = 'A';
+        boolean tBoolean = true;
+        byte tByte = 127;
+        short tShort = 32767;
+        int tInt = 2147483647;
+        long tLong = 2L;
+        float tFloat = 45.6F;
+        double tDouble = 7D;
+        LOG.debug("Data type - char : {}, boolean : {}, byte : {}, short : {},"
+                        + " int : {}, long : {}, float : {}, double : {}",
+                tChar, tBoolean, tByte, tShort, tInt, tLong, tFloat, tDouble);
+    }
+}
