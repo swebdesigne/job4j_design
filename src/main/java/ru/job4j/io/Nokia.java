f36d889fc3313d3a272d33ee479e1d0ab91f0ef2
beelk <swebdesigne@gmail.com>
2021-10-15 11:27:25 +0300

    Формат JSON
    Signed-off-by: beelk <swebdesigne@gmail.com>

--- src/main/java/ru/job4j/io/Nokia.java
+++ src/main/java/ru/job4j/io/Nokia.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Map;
 
 public class Nokia {
@@ -11,27 +12,32 @@ public class Nokia {
     private final SmartPhone hardware;
     private final int year;
     private float price;
+    private String[] colors;
 
-    public Nokia(String name, int year, float price, SmartPhone hardware) {
+    public Nokia(String name, int year, float price, SmartPhone hardware, String... color) {
         this.name = name;
         this.year = year;
         this.price = price;
         this.hardware = hardware;
+        this.colors = color;
     }
 
     @Override
     public String toString() {
         return "Nokia{"
                 + "name='" + name
+                + ", hardware=" + hardware
                 + ", year=" + year
                 + ", price=" + price
-                + ", hardware=" + hardware
+                + ", colors=" + Arrays.toString(colors)
                 + '}';
     }
 
     public static void main(String[] args) throws IOException {
-        Nokia phone = new Nokia("Nokia 2100", 2021, 40.21F,
-                new SmartPhone(3.400F, 128, true, Map.of("type", "IPS", "resolution", "2400x1080", "Hz", "120"))
+        Nokia phone = new Nokia(
+                "Nokia 2100", 2021, 40.21F,
+                new SmartPhone(3.400F, 128, true, Map.of("type", "IPS", "resolution", "2400x1080", "Hz", "120")),
+                "red", "black"
         );
         final Gson gson = new GsonBuilder().create();
         String data = gson.toJson(phone);
