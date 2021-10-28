b5ce763c7668269bf4fa7a82ac957b58f92f4ff6
Igor <swebdesigne@gmail.com>
2021-10-28 15:34:08 +0300

    Экзамен по IO

---
+++ src/main/java/ru/job4j/io/exam/ISearch.java
@@ -0,0 +1,7 @@
+package ru.job4j.io.exam;
+
+import java.nio.file.Path;
+
+public interface ISearch {
+    public boolean search(Path path);
+}
---
+++ src/main/java/ru/job4j/io/exam/Regex.java
@@ -0,0 +1,22 @@
+package ru.job4j.io.exam;
+
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+
+public class Regex implements ISearch {
+    String pattern;
+
+    public Regex(String pattern) {
+        this.pattern = pattern;
+    }
+
+    /**
+     * метод ищет файл по регулярному выражению, например - .*\\bot\\.*txt
+     * @param path - путь к файлу
+     * @return - возвращает true или false
+    */
+    @Override
+    public boolean search(Path path) {
+        return Pattern.compile(pattern).matcher(path.toString()).matches();
+    }
+}
