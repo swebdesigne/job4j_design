df721eb3cb3d3c7dc29874e1fb06a7890a39b5b1
beelk <swebdesigne@gmail.com>
2021-11-07 18:53:22 +0300

    Связи между таблицами

---
+++ src/main/java/ru/job4j/sql/relationship/one_to_one.sql
@@ -0,0 +1,10 @@
+create table addresses(
+	id serial primary key,
+	address varchar(255)
+);
+
+create table customers (
+	id serial primary key,
+	custmer_name varchar(255),
+	address_id int references addresses(id) unique
+);
