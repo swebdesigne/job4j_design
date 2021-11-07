5645ef7fb9cf98afacc353ff20b77c260a9fd17b
beelk <swebdesigne@gmail.com>
2021-11-07 12:14:26 +0300

    Сохранение и восстановление данных

---
+++ src/main/java/ru/job4j/mio/StoringAndRecoveringData.java
@@ -0,0 +1,33 @@
+package ru.job4j.mio;
+
+import java.io.*;
+
+public class StoringAndRecoveringData {
+    private void writer() {
+        try (DataOutputStream out = new DataOutputStream(
+                new BufferedOutputStream(new FileOutputStream("data.txt")));
+        ) {
+            out.writeDouble(3.123);
+            out.writeUTF("output into file data.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reader() {
+        try (DataInputStream in = new DataInputStream(
+                new BufferedInputStream(new FileInputStream("data.txt"))
+        )
+        ) {
+            System.out.println(in.readDouble());
+            System.out.println(in.readUTF());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        StoringAndRecoveringData file = new StoringAndRecoveringData();
+        file.writer();
+        file.reader();
+    }
+}
