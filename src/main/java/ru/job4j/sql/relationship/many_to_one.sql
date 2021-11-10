8b88b3be569022547608ca68c14513549d242571
beelk <swebdesigne@gmail.com>
2021-11-10 21:31:04 +0300

    Связи между таблицами

--- src/main/java/ru/job4j/sql/relationship/many_to_one.sql
+++ src/main/java/ru/job4j/sql/relationship/many_to_one.sql
@@ -1,14 +1,13 @@
 -- one-to-many
 -- писатель может иметь множество книг
 -- конкрентная книга принадлежит только одному писателю
+create table writer(
+	id serial primary key,
+	name_writer varchar(255),
+);
 
 create table books(
 	id serial primary key,
 	title_book varchar(255)
-);
-
-create table writer(
-	id serial primary key,
-	name_writer varchar(255),
-	book_id int references books(id)
+	book_id int references writer(id)
 );
