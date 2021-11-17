8fc2ffb893e7683f49f72772cd81548eb53de000
swebdesige <swebdesigne@gmail.com>
2021-11-17 19:57:14 +0300

    Чтение из стандартного потока ввода

--- src/main/java/ru/job4j/mio/Echo.java
+++ src/main/java/ru/job4j/mio/Echo.java
@@ -3,12 +3,11 @@ package ru.job4j.mio;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
 public class Echo {
     public static void main(String[] args) {
         try (
-                Reader r = new InputStreamReader(System.in);
+                InputStreamReader r = new InputStreamReader(System.in);
                 BufferedReader bf = new BufferedReader(r)
         ) {
             String s;
