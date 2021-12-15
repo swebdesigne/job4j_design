ce0beafced5642fe9b8fc3d9d71f34d6b103a43c
swebdesigne <swebdesigne@gmail.com>
2021-12-15 22:00:30 +0300

    Scanner

--- src/main/java/ru/job4j/mio/Scanner1.java
+++ src/main/java/ru/job4j/mio/Scanner1.java
@@ -13,8 +13,8 @@ public class Scanner1 {
         int y = scanner.nextInt();
         System.out.println("x / y: " + (x / y));
         System.out.println("Write the couple strings");
-        String str = scanner.nextLine();
-        System.out.println("String equals: " + str);
+        String str1 = scanner.nextLine();
+        System.out.println("String equals: " + str1);
         Scanner s = new Scanner("Hello my friend");
         Scanner str = s.useDelimiter(" ");
         System.out.println(str.next());
