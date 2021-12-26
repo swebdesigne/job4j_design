cce56bb7f917d87d7f291f3ce0b068c24a434dc8
swebdesigne <swebdesigne@gmail.com>
2021-12-26 12:10:10 +0300

    inner join

--- src/main/java/ru/job4j/sql/join/inner/inner_join.sql
+++ src/main/java/ru/job4j/sql/join/inner/inner_join.sql
@@ -17,7 +17,13 @@ insert into employee (name, surname, age) values ('Boris', 'Sivolobov', 35);
 insert into salary (salary) values (50);
 
 select * from employee join salary on employee.id = salary.id;
+
 select * from employee as e join salary as s on e.id = s.id;
+
 select e.name, e.age, s.salary from employee as e join salary as s on e.id = s.id;
-select e.name, e.age, s.salary from employee as e join salary as s on e.id = s.id where s.salary < 50;
-select e.name, e.age, s.salary from employee as e join salary as s on e.id = s.id where e.age > 34 and s.salary < 50;
+
+select e.name, e.age, s.salary from employee as e join salary as s
+on e.id = s.id where s.salary < 50;
+
+select e.name, e.age, s.salary from employee as e join salary as s
+on e.id = s.id where e.age > 34 and s.salary < 50;
