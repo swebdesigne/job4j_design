12ea20a036e1892ad7f9a7238680c15b959a61d4
swebdesigne <swebdesigne@gmail.com>
2022-01-04 15:49:33 +0300

    Группировка и агрегатные функции

--- src/main/java/ru/job4j/sql/agregat_function.sql
+++ src/main/java/ru/job4j/sql/agregat_function.sql
@@ -64,7 +64,7 @@ from devices_people as dp
 join people p
 on dp.people_id = p.id
 join devices as d on dp.device_id = d.id
-group by p.name having avg(d.price) > 6000;
+group by p.name having avg(d.price) > 5000;
 
 
 
