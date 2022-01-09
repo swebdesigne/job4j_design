ff0e64991a43bad9121cd49e87e6b6f709188b56
swebdesigne <swebdesigne@gmail.com>
2022-01-09 19:11:05 +0300

    Statement

--- src/main/java/ru/job4j/sql/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/sql/jdbc/TableEditor.java
@@ -1,8 +1,6 @@
 package ru.job4j.sql.jdbc;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
